1a44e381d1f93ac6ad4ce693a968346931d4f6e3
18xiaolaohu <3190213847@qq.com>
2022-06-27 11:35:19 +0800

    项目创建完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Layout from '@/views/Layout'
+const Home = () => import('@/views/Home')// 首页
+const Seek = () => import('@/views/Seek')// 找房
+const Counsel = () => import('@/views/Counsel')// 咨询
+const My = () => import('@/views/My')// 我的
+const Login = () => import('@/views/Login')// 登录
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: Layout,
+    redirect: '/home',
+    children: [
+      { path: 'home', component: Home },
+      { path: 'seek', component: Seek },
+      { path: 'counsel', component: Counsel },
+      { path: 'my', component: My }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item replace to="/home" icon="home-o"
+        >首页
+        <template #icon>
+          <van-icon name="wap-home-o" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/seek" icon="search"
+        >找房
+        <template #icon>
+          <van-icon name="search" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/counsel" icon="friends-o"
+        >咨询
+        <template #icon>
+          <van-icon name="description" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/my" icon="setting-o"
+        >我的
+        <template #icon>
+          <van-icon name="user-o" />
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  created () { },
+  data () {
+    return {
+      active: 0
+    }
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped lang='less'>
+.van-icon {
+  font-size: 25px;
+}
+/deep/.van-tabbar-item__text {
+  font-size: 15px;
+}
+</style>
